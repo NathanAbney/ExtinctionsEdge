db600d273869830482973c6c862cede409dd4755
Nathan Abney <80005110+NathanAbney@users.noreply.github.com>
2023-06-07 18:50:36 -0400

    Add files via upload

--- Global.gd
+++ Global.gd
@@ -8,6 +8,9 @@ var god_mode : bool = false
 var current_level : int = 0
 var next_level : String
 var boss_rush : bool = false
+var no_hit : bool = false
+var game_beat : bool = false
+var boss_rush_beaten : bool = false
 
 func change_next_level():
 	var rng = RandomNumberGenerator.new()
@@ -17,37 +20,44 @@ func change_next_level():
 			var room = rng.randi_range(1,4)
 			if room == 1:
 				next_level = "res://Rooms/Small/SmallRoom2.tscn"
-			if room == 2:
+			elif room == 2:
 				next_level = "res://Rooms/Small/SmallRoom3.tscn"
-			if room == 3:
+			elif room == 3:
 				next_level = "res://Rooms/Small/SmallRoom4.tscn"
-			if room == 4:
+			elif room == 4:
 				next_level = "res://Rooms/Small/SmallRoom5.tscn"
 		if (current_level == 2 or current_level == 4): # Medium rooms
 			var room = rng.randi_range(1,2)
 			if room == 1:
 				next_level = "res://Rooms/Medium/MediumRoom1.tscn"
-			if room == 2:
+			elif room == 2:
 				next_level = "res://Rooms/Medium/MediumRoom2.tscn"
 		if current_level == 5: # Large rooms
-			next_level = "res://Rooms/Large/LargeRoom1.tscn"
-		if current_level == 6: # Boss rooms
+			var room = rng.randi_range(1,2)
+			if room == 1:
+				next_level = "res://Rooms/Large/LargeRoom2.tscn"
+			elif room == 2:
+				next_level = "res://Rooms/Large/LargeRoom1.tscn"
+		if current_level == 6: # Wave battle
+			next_level = "res://Rooms/Boss/Wave.tscn"
+		if current_level == 7: # Boss rooms
 			var room = rng.randi_range(1,4)
 			MusicController.play_music(2)
 			if room == 1:
 				next_level = "res://Rooms/Boss/Boss1.tscn"
-			if room == 2:
+			elif room == 2:
 				next_level = "res://Rooms/Boss/Boss2.tscn"
-			if room == 3:
+			elif room == 3:
 				next_level = "res://Rooms/Boss/Boss3.tscn"
-			if room == 4:
+			elif room == 4:
 				next_level = "res://Rooms/Boss/Boss4.tscn"
 	else:
 		if current_level == 1:
 			next_level = "res://Rooms/Boss/Boss3.tscn"
-		if current_level == 2:
+		elif current_level == 2:
 			next_level = "res://Rooms/Boss/Boss2.tscn"
-		if current_level == 3:
+		elif current_level == 3:
 				next_level = "res://Rooms/Boss/Boss1.tscn"
-		if current_level == 4:
+		elif current_level == 4:
+			boss_rush_beaten = true
 			next_level = "res://Scenes/main_menu.tscn"
