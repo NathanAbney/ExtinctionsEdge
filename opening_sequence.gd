ae5eca1803243e667dfea9d144104a1598fa5ffc
Nathan Abney <80005110+NathanAbney@users.noreply.github.com>
2023-06-09 23:45:00 -0400

    Updating With Local Directory (3/3)

--- opening_sequence.gd
+++ opening_sequence.gd
@@ -1,5 +1,21 @@
 extends Node2D
 
+func _ready():
+	MusicController.play_music(6)
+
+func _process(delta):
+	var config = ConfigFile.new()
+	var error = config.load("user://character_unlock.cfg")
+	if error == OK:
+		if !config.get_value("Settings","Fullscreen",true):
+			DisplayServer.window_set_mode(0)
+		else:
+			DisplayServer.window_set_mode(3)
+	if error != OK:
+		DisplayServer.window_set_mode(3)
+	if (Input.is_action_just_pressed("Space")):
+		_on_timer_timeout()
+
 func _on_timer_timeout():
 	var config = ConfigFile.new()
 	var error = config.load("user://character_unlock.cfg")
